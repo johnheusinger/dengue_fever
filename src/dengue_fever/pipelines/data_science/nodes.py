057307624b7ed285119ae7385c352793f3739c95
John Heusinger <john.heusinger@gmail.com>
2024-05-03 18:39:02 +0200

    Use RandomForestRegressor

--- src/dengue_fever/pipelines/data_science/nodes.py
+++ src/dengue_fever/pipelines/data_science/nodes.py
@@ -1,31 +1,11 @@
-import logging
-from typing import Dict, Tuple
-
+from typing import Tuple
+import numpy as np
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import r2_score
-from sklearn.model_selection import train_test_split
+# fix the line below
+from sklearn.ensemble import RandomForestRegressor
 
-
-def split_data(data: pd.DataFrame, parameters: Dict) -> Tuple:
-    """Splits data into features and targets training and test sets.
-
-    Args:
-        data: Data containing features and target.
-        parameters: Parameters defined in parameters/data_science.yml.
-    Returns:
-        Split data.
-    """
-    X = data[parameters["features"]]
-    y = data["price"]
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=parameters["test_size"], random_state=parameters["random_state"]
-    )
-    return X_train, X_test, y_train, y_test
-
-
-def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> LinearRegression:
-    """Trains the linear regression model.
+def train_model(data: pd.DataFrame, model_options) -> RandomForestRegressor:
+    """Trains the random forest regression model.
 
     Args:
         X_train: Training data of independent features.
@@ -33,14 +13,27 @@ def train_model(X_train: pd.DataFrame, y_train: pd.Series) -> LinearRegression:
 
     Returns:
         Trained model.
+
+        RandomForestRegressor(n_estimators=200,max_depth=7,min_samples_leaf=4,min_samples_split=5)
     """
-    regressor = LinearRegression()
-    regressor.fit(X_train, y_train)
+    # Filter out test data and drop data_type column
+    data = data[data['data_type'] == 'train']
+    data = data.drop('data_type', axis=1)
+
+    X = data.drop('total_cases',axis=1)
+    y = data['total_cases']
+
+    regressor = RandomForestRegressor(n_estimators=model_options['n_estimators'],
+        max_depth=model_options['max_depth'],min_samples_leaf=model_options['min_samples_leaf'],
+        min_samples_split=model_options['min_samples_split'])
+
+    regressor.fit(X,y)
+
     return regressor
 
 
-def evaluate_model(
-    regressor: LinearRegression, X_test: pd.DataFrame, y_test: pd.Series
+''' def evaluate_model(
+    regressor: RandomForestRegressor, X_test: pd.DataFrame, y_test: pd.Series
 ):
     """Calculates and logs the coefficient of determination.
 
@@ -52,4 +45,27 @@ def evaluate_model(
     y_pred = regressor.predict(X_test)
     score = r2_score(y_test, y_pred)
     logger = logging.getLogger(__name__)
-    logger.info("Model has a coefficient R^2 of %.3f on test data.", score)
+    logger.info("Model has a coefficient R^2 of %.3f on test data.", score) '''
+
+def make_prediction(
+    data: pd.DataFrame, regressor: RandomForestRegressor, submission_data: pd.DataFrame
+) -> Tuple[pd.DataFrame, pd.Series]:
+    """Make predictions using the trained model.
+
+    Args:
+        data: Data on which to make predictions.
+        regressor: Trained model.
+
+    Returns:
+        A tuple of the input data and the predictions.
+    """
+    # Filter out test data and drop data_type column
+    data = data[data['data_type'] == 'test']
+    data = data.drop(['data_type', 'total_cases'], axis=1)
+
+    y_pred = np.round(regressor.predict(data))
+
+    submission_data['total_cases'] = y_pred.astype(int)
+
+    return submission_data
+
