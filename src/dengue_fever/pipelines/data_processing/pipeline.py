1115f0fd4f8d3526ad3beb89011823b011dd5a56
John Heusinger <john.heusinger@gmail.com>
2024-05-03 15:55:45 +0200

    Add nodes

--- src/dengue_fever/pipelines/data_processing/pipeline.py
+++ src/dengue_fever/pipelines/data_processing/pipeline.py
@@ -1,28 +1,40 @@
 from kedro.pipeline import Pipeline, node, pipeline
 
-from .nodes import create_model_input_table, preprocess_companies, preprocess_shuttles
+from .nodes import merge_features_and_label, encoding, dropping_columns, impute_missing_values, scale
 
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
             node(
-                func=preprocess_companies,
-                inputs="companies",
-                outputs="preprocessed_companies",
-                name="preprocess_companies_node",
+                func=merge_features_and_label,
+                inputs=["dengue_features_train", "dengue_features_test", "dengue_labels_train"],
+                outputs="merged_features_and_label",
+                name="merge_features_and_label",
             ),
             node(
-                func=preprocess_shuttles,
-                inputs="shuttles",
-                outputs="preprocessed_shuttles",
-                name="preprocess_shuttles_node",
+                func=encoding,
+                inputs="merged_features_and_label",
+                outputs="encoded_features_and_label",
+                name="encoding",
+            ),         
+            node(
+                func=dropping_columns,
+                inputs=["encoded_features_and_label", "params:columns_to_drop"],
+                outputs="dropped_features_and_label",
+                name="dropping_columns",
             ),
             node(
-                func=create_model_input_table,
-                inputs=["preprocessed_shuttles", "preprocessed_companies", "reviews"],
-                outputs="model_input_table",
-                name="create_model_input_table_node",
+                func=impute_missing_values,
+                inputs=["dropped_features_and_label"],
+                outputs="imputed_values",
+                name="impute_missing_values",
             ),
+            node(
+                func=scale,
+                inputs=["imputed_values"],
+                outputs="scaled_values",
+                name="scale",
+            )
         ]
     )
